b008406ce69c4770de83dd7a2e3b5929dc2c1077
Subham <sahasubham406@gmail.com>
2024-08-22 21:11:44 +0530

    Initial commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,29 +1,43 @@
 import { Box, Image, Text } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import React from "react";
-import btcLogo from "../Assets/btc.png";
+import btcSrc from "../Assets/btc.png";
 
 const Home = () => {
-  return <Box bgColor={"blackAlpha.900"} w={"full"} h={"90vh"} overflow="hidden">
+  return (
+    <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"} overflow={"hidden"}>
+      <motion.div
+        style={{
+          height: "80vh",
+        }}
+        animate={{
+          translateY: "80px",
+        }}
+        transition={{
+          duration: 1.8,
+          repeat: Infinity,
+          repeatType: "reverse",
+        }}
+      >
+        <Image
+          w={"full"}
+          h={"full"}
+          objectFit={"contain"}
+          src={btcSrc}
+        />
+      </motion.div>
 
-    <motion.div style={{
-      height: "80vh",
-    }}
-    animate = {{
-      rotate: [0, 360],
-    }}
-
-    transition={{
-      duration:40,
-      repeat: Infinity,
-      repeatType: "reverse"
-    }}
-    >
-      <Image w={"full"} h={"full"} objectFit={"contain"} src={btcLogo} />
-    </motion.div>
-
-    <Text fontSize={"6xl"} textAlign={"center"} fontWeight={"thin"} color={"whiteAlpha.700"} mt={["-20","-25"]}>Xcrypto</Text>
-  </Box>;
+      <Text
+        fontSize={"6xl"}
+        textAlign={"center"}
+        fontWeight={"thin"}
+        color={"whiteAlpha.700"}
+        mt={"-20"}
+      >
+        Xcrypto
+      </Text>
+    </Box>
+  );
 };
 
 export default Home;
